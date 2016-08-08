8ad6d6797801b1710095b4f0ce4a436239486529
Magnus-The-Red <Magnus-The-Red@users.noreply.github.com>
2016-08-08 22:37:02 +0200

    k

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+<<<<<<< HEAD
 	console.log('show me your meaty log, please');
 	//var back_prices = $('.last-back-cell .bet-button-price').map(function(){return this.innerHTML;}).toArray();
 	//var lay_prices = $('.first-lay-cell .bet-button-price').map(function(){return this.innerHTML;}).toArray();
@@ -25,3 +26,24 @@ xhttp.open("GET", "https://www.betfair.com/exchange/plus/#/football/market/1.126
 //}
 	// console.log(back_prices)
 	});
+=======
+	console.log('show me your log')
+	
+	// ## extract values of interest from page	
+	var prices = $('.last-back-cell .bet-button-price').map(function(){return this.innerHTML;}).toArray();
+
+	// console.log(prices);
+
+	var port = chrome.runtime.connect({'name':'betfair'});
+
+	port.postMessage({'prices':prices});
+});
+
+// ## hoarding
+	// var prices = $('.last-back-cell .bet-button-price').contents()[1];
+
+	// chrome.runtime.sendMessage({'sven':'judge me by the content of my script'});
+	// chrome.runtime.sendMessage({'sven': chrome.windows.WINDOW_ID_CURRENT});
+
+	// console.log(['the port',port]);
+>>>>>>> origin/master
